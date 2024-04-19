271872ebd28a5de1248111cf54ebb2139d8cdf52
Hillol18 <barikhan.hillol@gmail.com>
2024-04-20 00:59:29 +0600

    Update Five-Star Sellers.cpp
    refactored description

--- Five-Star Sellers.cpp
+++ Five-Star Sellers.cpp
@@ -1,7 +1,11 @@
 /*
 Problem link:  https://aonecode.com/amazon-online-assessment-five-star-sellers
 
-Third-party companies that sell their products on Amazon.com are able to analyze the customer reviews for their products in real time. Imagine that Amazon is creating a category called "five-star sellers" that will only display products sold by companies whose average percentage of five-star reviews per-product is at or above a certain threshold. Given the number of five-star and total reviews for each product a company sells, as well as the threshold percentage, what is the minimum number of additional fivestar reviews the company needs to become a five-star seller?
+Third-party companies that sell their products on Amazon.com are able to analyze the customer reviews for their products in real time.
+Imagine that Amazon is creating a category called "five-star sellers" that will only display products sold by companies whose average 
+percentage of five-star reviews per-product is at or above a certain threshold. Given the number of five-star and total reviews for each 
+product a company sells, as well as the threshold percentage, what is the minimum number of additional fivestar reviews the company needs 
+to become a five-star seller?
 
 For example, let's say there are 3 products (n = 3) where productRatings = [[4,4], [1,2], [3, 6]], and the percentage ratings Threshold = 77. The first number for each product in productRatings denotes the number of fivestar reviews, and the second denotes the number of total reviews. Here is how we can get the seller to reach the threshold with the minimum number of additional five-star reviews:
 
